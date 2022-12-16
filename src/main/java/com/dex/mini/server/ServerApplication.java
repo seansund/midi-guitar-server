2adbe78623812352f0ed3e9ce7694d69c226469e
Sean Sundberg <seansund@us.ibm.com>
2022-12-16 13:44:12 -0600

    Adds graphql service framework
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- src/main/java/com/dex/mini/server/ServerApplication.java
+++ src/main/java/com/dex/mini/server/ServerApplication.java
@@ -1,13 +1,26 @@
 package com.dex.mini.server;
 
+import com.dex.mini.server.driver.Driver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServerApplication {
+public class ServerApplication implements ApplicationRunner {
+
+	@Autowired
+	private Driver driver;
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ServerApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		driver.run();
 	}
 
 }
