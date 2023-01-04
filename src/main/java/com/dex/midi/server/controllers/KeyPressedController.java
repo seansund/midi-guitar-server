031c2675fbbe7879eb85522d5d58156084796ab6
Sean Sundberg <seansund@us.ibm.com>
2023-01-04 09:28:29 -0600

    Adds KeyPressedController
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

---
+++ src/main/java/com/dex/midi/server/controllers/KeyPressedController.java
@@ -0,0 +1,33 @@
+package com.dex.midi.server.controllers;
+
+import com.dex.midi.server.model.ChordLabel;
+import com.dex.midi.server.repository.KeyPressRepository;
+import io.reactivex.rxjava3.core.BackpressureStrategy;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SubscriptionMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import reactor.adapter.rxjava.RxJava3Adapter;
+import reactor.core.publisher.Flux;
+
+import java.util.Collection;
+
+@Controller
+@CrossOrigin
+public class KeyPressedController {
+    private final KeyPressRepository repo;
+
+    public KeyPressedController(KeyPressRepository repo) {
+        this.repo = repo;
+    }
+
+    @QueryMapping
+    public String getLastKeyPressed() {
+        return repo.getLastKeyPressed();
+    }
+
+    @SubscriptionMapping
+    public Flux<String> keyPressed() {
+        return RxJava3Adapter.observableToFlux(repo.keysPressed(), BackpressureStrategy.LATEST);
+    }
+}
